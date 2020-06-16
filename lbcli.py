d4b6a8b8bfe74138b161e6bc9502bda91be6ee71
razaborg <contact@razaborg.fr>
2020-06-17 00:01:01 +0200

    added city parameter + price-per-square post-filter

--- lbcli.py
+++ lbcli.py
@@ -3,10 +3,12 @@ import argparse
 import pylbc
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--coordinates", '-c', nargs=2, metavar=('LAT', 'LNG'), type=float, help='Lat/Long of the center point.')
+parser.add_argument("--coordinates", nargs=2, metavar=('LAT', 'LNG'), type=float, help='Lat/Long of the center point.')
+parser.add_argument("--city", nargs=2, metavar=('CITY', 'POSTCODE'), type=str, help='City and Postcode to seek in.')
 parser.add_argument('--radius', type=int, help="Radius around the center point.", default=50)
 parser.add_argument('--category', '-C', type=str, choices=pylbc.CATEGORIES.values(), required=True)
 parser.add_argument('--real-estate-type', '-T', type=str, choices=pylbc.REAL_ESTATE_TYPES.values(), required=True)
+parser.add_argument('--max-price-per-square', type=int, help='Set the maximum price/m2. WARNING this is a *post*-filter. Can be long to process if too many entries.')
 parser.add_argument('--price-range', '-p', nargs=2, type=int)
 parser.add_argument('--square-range', '-s', nargs=2, type=int)
 parser.add_argument('--rooms-range', '-r', nargs=2, type=int)
@@ -23,6 +25,10 @@ query.set_real_estate_types([args.real_estate_type])
 if args.coordinates:
     query.set_coordinates(lat=args.coordinates[0], lng=args.coordinates[1], radius=args.radius)
 
+if args.city:
+    query.add_city(args.city[0], args.city[1])
+
+
 if args.price_range:
     query.set_price(mini=args.price_range[0], maxi=args.price_range[1])
 if args.rooms_range:
@@ -37,8 +43,12 @@ if args.verbose:
     query.show_filters()
 
 infos = query.request_infos()
-print("{} results found matching your query.".format(infos['total']))
+print("{} results will be downloaded from the server.".format(infos['total']))
 if not args.y:
     input('Press any key to continue or hit "Ctrl-C" to quit:')
 for result in query.iter_results():
-    print(result)
+    if args.max_price_per_square:
+        if result.price_per_square < args.max_price_per_square:
+            print(result)
+    else:
+        print(result)
